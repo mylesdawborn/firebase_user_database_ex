cdbc5b6108583411081ec4e382f60a500c1689d8
Myles Dawborn <33122420+mylesdawborn@users.noreply.github.com>
2021-03-08 21:46:14 +0200

    Warning: Written pre 1.2 Flutter version. (Database used: Firebase. Warning: Database may have been deleted due to no activity.

---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+final FirebaseOptions firebaseOptionsApp = FirebaseOptions(
+  googleAppID: '1:311276605059:android:1b5a7dcfb1658f5f',
+  apiKey: 'AIzaSyDe7lAbb5hAmahxqMjH-irxiAFQBnA280Y',
+  databaseURL: 'https://fir-user-database.firebaseio.com',
+);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase User Database Example',
+      theme: ThemeData.dark(),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Item> items = List();
+  Item item;
+  DatabaseReference itemRef;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    item = Item('', '');
+    itemRef = FirebaseDatabase.instance.reference().child('items');
+    itemRef.onChildAdded.listen(_onEntryAdded);
+    itemRef.onChildChanged.listen(_onEntryChanged);
+  }
+
+  _onEntryAdded(Event event) {
+    setState(() {
+      items.add(Item.fromSnapshot(event.snapshot));
+    });
+  }
+
+  _onEntryChanged(Event event) {
+    var old = items.singleWhere((entry) {
+      return entry.key == event.snapshot.key;
+    });
+    setState(() {
+      items[items.indexOf(old)] = Item.fromSnapshot(event.snapshot);
+    });
+  }
+
+  void handleSubmit() {
+    final FormState form = formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+      form.reset();
+      itemRef.push().set(item.toJson());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Example'),
+      ),
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 0,
+            child: Center(
+              child: Form(
+                key: formKey,
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.info),
+                      title: TextFormField(
+                        initialValue: '',
+                        onSaved: (val) => item.title = val,
+                        validator: (val) => val == '' ? val : null,
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.info),
+                      title: TextFormField(
+                        initialValue: '',
+                        onSaved: (val) => item.body = val,
+                        validator: (val) => val == '' ? val : null,
+                      ),
+                    ),
+                    IconButton(
+                        icon: Icon(Icons.send),
+                        onPressed: () {
+                          handleSubmit();
+                        }),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            child: FirebaseAnimatedList(
+              query: itemRef,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                  Animation<double> animation, int index) {
+                return ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text(items[index].title),
+                  subtitle: Text(items[index].body),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Item {
+  String key;
+  String title;
+  String body;
+
+  Item(this.title, this.body);
+
+  Item.fromSnapshot(DataSnapshot snapshot)
+      : key = snapshot.key,
+        title = snapshot.value["title"],
+        body = snapshot.value["body"];
+
+  toJson() {
+    return {
+      "title": title,
+      "body": body,
+    };
+  }
+}
